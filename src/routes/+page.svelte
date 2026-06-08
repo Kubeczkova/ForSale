46eb69affcbde2ea65dec461ec5d247d295b6a98
Nikola Kubeczkova <nikola.kubeczkova@olc.cz>
2026-06-08 19:26:38 +0200

    visual

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,10 @@
 	import Contact from '$lib/components/Contact.svelte';
 	import Gallery from '$lib/components/Gallery.svelte';
 	import BidForm from '$lib/components/BidForm.svelte';
-
-	function scrollToBidForm() {
-		document.getElementById('prihodit')?.scrollIntoView({ behavior: 'smooth' });
-	}
 </script>
 
 <div class="bg-surface">
-	<Hero
-		imageSrc=""
-		address="Hnojník 383"
-		size="3+kk · 78 m²"
-		plot="Pozemek 450 m²"
-		currentBid="2 500 000 Kč"
-		onBid={scrollToBidForm}
-	/>
+	<Hero/>
 
 	<Contact />
 
